4716de14d0d0f2a10a2cbcd7e62735519b140e35
Manuel <carlosd99m@gmail.com>
2021-04-21 11:21:09 -0600

    busqueda ✨

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,38 +1,50 @@
-import {usePokemon} from "../hooks/usePokemon";
-import {Loading} from "../components/Loading";
-import {Pokemon} from "../interfaces/fetchAllPokemonResponse";
-import {useState} from "react";
+import { usePokemon } from "../hooks/usePokemon";
+import { Loading } from "../components/Loading";
+import { Pokemon } from "../interfaces/fetchAllPokemonResponse";
+import { useState } from "react";
 
 export const HomePage = () => {
+  const { isLoading, pokemons } = usePokemon();
+  const [currentPage, setCurrentPage] = useState(0);
+  const [search, setSearch] = useState("");
 
-    const {isLoading, pokemons} = usePokemon();
-    const [ currentPage, setCurrentPage ] = useState(0)
-    const [ search, setSearch ] = useState('')
+  const filteredPokemons = (): Pokemon[] => {
+    if (search.length === 0)
+      return pokemons.slice(currentPage, currentPage + 5);
 
-    const filteredPokemons = (): Pokemon[] => {
-        return  pokemons.slice(currentPage, currentPage + 5)
-    }
+    // Si hay algo en l caja de texto de
+    const filtered = pokemons.filter((poke) => poke.name.includes(search));
+    return filtered.slice(currentPage, currentPage + 5);
+  };
 
-    const nextPage = () => {
-        setCurrentPage( currentPage + 5)
+  const nextPage = () => {
+    if (
+      pokemons.filter((poke) => poke.name.includes(search)).length >
+      currentPage + 5
+    ) {
+      setCurrentPage(currentPage + 5);
     }
-    const previousPage = () => {
-        if (currentPage > 0)
-            setCurrentPage( currentPage - 5)
-    }
+  };
+  const previousPage = () => {
+    if (currentPage > 0) setCurrentPage(currentPage - 5);
+  };
+
+  const onSearchChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentPage(0);
+    setSearch(target.value);
+  };
 
   return (
     <div className="mt-5">
-
       <h1>Listado de Pokémoms</h1>
       <hr />
-
-      <input 
-        type="text" 
+      <input
+        type="text"
         className="mb-5 form-control"
         placeholder="Buscar Pokémom"
+        value={search}
+        onChange={onSearchChange}
       />
-
       <table className="table">
         <thead>
           <tr>
@@ -41,45 +53,26 @@ export const HomePage = () => {
             <th>Imagen</th>
           </tr>
         </thead>
-          <tbody>
-          {
-              filteredPokemons().map( ({ id, name, pic }) => (
-                  <tr key={ id }>
-                      <td> { id } </td>
-                      <td> { name } </td>
-                      <td>
-                        <img
-                          src={ pic }
-                          alt={ name }
-                          style={{height: 75}}
-                        />
-                      </td>
-                  </tr>
-              ))
-          }
-
-          </tbody>
+        <tbody>
+          {filteredPokemons().map(({ id, name, pic }) => (
+            <tr key={id}>
+              <td> {id} </td>
+              <td> {name} </td>
+              <td>
+                <img src={pic} alt={name} style={{ height: 75 }} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
-
-        <button 
-            className="btn btn-primary"
-            onClick={ previousPage }    
-        >
-            Anteriores
-        </button>
-        &nbsp;
-        <button
-            className="btn btn-primary"
-            onClick={ nextPage }
-        >
-            Siguientes
-        </button>
-
-
-        {
-            isLoading && <Loading />
-        }
-
+      <button className="btn btn-primary" onClick={previousPage}>
+        Anteriores
+      </button>
+      &nbsp;
+      <button className="btn btn-primary" onClick={nextPage}>
+        Siguientes
+      </button>
+      {isLoading && <Loading />}
     </div>
   );
 };
