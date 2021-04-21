6279f138ad8b4c940ffa582bece6ebf686252af9
Manuel <carlosd99m@gmail.com>
2021-04-20 23:35:33 -0600

    add api

---
+++ src/hooks/usePokemon.tsx
@@ -0,0 +1,13 @@
+import {useState} from "react";
+
+export const usePokemon = () => {
+
+    const [isLoading, setisLoading] = useState(true);
+
+        
+
+    return (
+        isLoading
+    )
+
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,24 @@
+export const HomePage = () => {
+
+    
+
+
+  return (
+    <div className="mt-5">
+
+      <h1>Listado de Pokémoms</h1>
+      <hr />
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Imagen</th>
+          </tr>
+        </thead>
+      </table>
+    
+    </div>
+  );
+};
